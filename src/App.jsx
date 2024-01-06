1ea8cc068355f0b67eacee86053bcef7366d694b
yashh246 <syash0582@icloud.com>
2024-01-06 14:53:29 +0530

    errror

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
 import React from "react";
 import "./App.css";
-import Header from "./Header";
-import AddContact from "./AddContact";
-import ContactList from "./ContactList";
+// import Header from "./Header.jsx";
+// import AddContact from "./AddContact.jsx"
+// import ContactList from "./ContactList.jsx"
 
 function App() {
-
-const contacts = [
-  {
-    id: "1",
-    name:"Yash",
-    email:"syash0582@gmail.com",
-  },
-  {
-    id: "2",
-    name:"aashu",
-    email:"aashu2@gmail.com",
-  },
-];
-  return (
-   <div className="ui container">
-    <Header />
-    <AddContact />
-    <ContactList contacts={contacts}  />
-   </div>
-  );
+    // const contacts = [
+    //   {
+    //     id:"1",
+    //     name:"Yash",
+    //     email:"syash0582@gmail.com",
+    //   },
+    //   {
+    //     id: "2",
+    //     name:"aashu",
+    //     email:"aashu2@gmail.com",
+    //   }
+    // ];
+    return (
+     <div className="ui container">
+      <h1>hello</h1>
+      {/* <Header />
+      <AddContact />
+      <ContactList contacts={contacts}  /> */}
+     </div>)
 }
 
 export default App
--- src/Header.jsx
+++ src/Header.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 const Header = () => {
     return (
-        <div className="ui fixed menu">
-            <div className="ui container center">
-                <h2>Contact Manager</h2>
+        <>
+            <div className="ui fixed menu">
+                <div className="ui container center">
+                    <h2>Contact Manager</h2>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
